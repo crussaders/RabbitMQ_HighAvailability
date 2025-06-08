7b6203df33974b7d61a2560691986bd1c7f44d3c
manishpandey <pandeym321@gmail.com>
2025-06-08 22:40:22 +0530

    made changes for failover

--- src/main/java/com/highAvailability/RabbitMQ/config/RabbitMQConfig.java
+++ src/main/java/com/highAvailability/RabbitMQ/config/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.highAvailability.RabbitMQ.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,5 +32,13 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding(Queue haQueue, DirectExchange haExchange) {
         return BindingBuilder.bind(haQueue).to(haExchange).with(routingKey);
+    }
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMissingQueuesFatal(false); // important for HA failover
+        factory.setRecoveryInterval(5000L);   // 5 seconds retry
+        return factory;
     }
 }
